af1d4ee9f1f24f1ae9edb526730cbdaccf008a40
takeuchisatoshi <satsitt3@gmail.com>
2019-03-10 09:59:39 +0900

    choiceメソッドを使用

--- omikuji.py
+++ omikuji.py
@@ -6,6 +6,8 @@ import random
 
 omikuji = ["大大吉", "大吉", "凶後大吉", "凶後吉", "末大吉", "末吉", "向大吉", "吉", "中吉", "小吉", "小凶後吉", "後吉", "吉凶未分末大吉", "吉凶不分末吉", "吉凶相半", "吉凶相交末吉", "吉凶相央"]
 
+# print(len(omikuji))
+
 """
 伏見稲荷大社 式
 大大吉・大吉・凶後大吉・凶後吉・末大吉・末吉・向大吉・吉・中吉・小吉・小凶後吉・後吉・吉凶未分末大吉・吉凶不分末吉・吉凶相半・吉凶相交末吉・吉凶相央
@@ -14,6 +16,8 @@ omikuji = ["大大吉", "大吉", "凶後大吉", "凶後吉", "末大吉", "末
 command + R
 """
 
-idx = random.randint(0, len(omikuji) - 1)
+# idx = random.randint(0, len(omikuji) - 1)
+#
+# print(omikuji[idx])
 
-print(omikuji[idx])
+print(random.choice(omikuji))
